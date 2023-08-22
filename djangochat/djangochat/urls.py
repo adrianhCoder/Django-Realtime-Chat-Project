618d470eccb134d21a488e37eabdc1131f9fd89d
Adrianh De Lucio <134009848+adrianhCoder@users.noreply.github.com>
2023-08-22 13:52:24 -0500

    urls fixed

--- djangochat/djangochat/urls.py
+++ djangochat/djangochat/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('rooms/',include('room.urls')),
+    path('rooms/', include('room.urls')),
     path('admin/', admin.site.urls),
 
 ]
